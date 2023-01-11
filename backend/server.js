d674de949c1905e28d9b2863a2f18a168d6f927a
Arun-Raj-B <sent2arunraj@gmail.com>
2023-01-11 20:56:27 +0530

    heroku on the way

--- backend/server.js
+++ backend/server.js
@@ -6,23 +6,33 @@ const adminRoutes = require("./routes/adminRoutes");
 const noteRoutes = require("./routes/notesRoutes");
 const connectDB = require("./config/db");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+const path = require("path");
 
 const app = express();
 dotenv.config();
 connectDB();
 app.use(express.json());
 
-// app.get("/", (req, res) => {
-//   res.send("API is running...");
-// });
-
-// app.get("/api/notes", (req, res) => {
-//   res.json(notes);
-// });
-
 app.use("/api/users", userRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/notes", noteRoutes);
+
+// --------------------------deployment------------------------------
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+// --------------------------deployment------------------------------
+
 app.use(notFound);
 app.use(errorHandler);
 
